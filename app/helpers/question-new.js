717e8c626c2a9606a30ceb9b8186ed5f082dfa1b
Joanna Anderson <joanna.saerom.anderson@gmail.com>
2016-11-05 16:13:40 -0700

    Adds helper to show questions asked today

--- app/helpers/question-new.js
+++ app/helpers/question-new.js
@@ -1,7 +1,12 @@
 import Ember from 'ember';
 
-export function questionNew(params/*, hash*/) {
-  return params;
+export function questionNew(params) {
+  var today = new Date();
+  var questionDate = new Date(params[0]);
+
+  if(today.getFullYear() === questionDate.getFullYear() && today.getMonth() === questionDate.getMonth() && today.getDate() === questionDate.getDate()) {
+    return new Ember.Handlebars.SafeString('<span class="red">New</span>');
+  }
 }
 
 export default Ember.Helper.helper(questionNew);
