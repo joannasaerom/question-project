ce09f6948dafdcde208b6ee69c5c3c4b62f05bdf
Joanna Anderson <joanna.saerom.anderson@gmail.com>
2016-11-05 17:10:51 -0700

    Adds computed property to show which questions have five + answers

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  zeroAnswer: Ember.computed('question.answers', function(){
+    if(this.get('question.answers.length') >= 5){
+      return new Ember.String.htmlSafe('<i class="glyphicon glyphicon-fire"></i>');
+    }
+  }),
 });
